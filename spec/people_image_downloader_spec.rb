d660eb41a7b940c2f710b420392c6b99a739cef3
lachie <lachiec@gmail.com>
2009-06-13 18:50:08 +1000

    PeopleImageDownloader#extract_image spec

--- spec/people_image_downloader_spec.rb
+++ spec/people_image_downloader_spec.rb
@@ -8,14 +8,23 @@ describe PeopleImageDownloader do
 <div class="box"><img title="RUDDOCK, the Hon. Philip Maxwell" alt="RUDDOCK, the Hon. Philip Maxwell" style="float: left; margin-top: 10px; margin-right: 10px; margin-bottom: 10px;" src="/parlInfo/download/handbook/allmps/0J4/upload_ref_binary/0j4.jpg"/><span class="sumLink">Biography for <span class="highlight"><strong>RUDDOCK</strong></span>, the Hon. <span class="highlight"><strong>Philip</strong></span> Maxwell</span><br/></div>
     EOF
 
-    o = Object.new
-    stub(Configuration).new {o}
-    stub(o).html_cache_path {'/tmp'}
+    stub(Configuration).new.stub!
 
-    @downloader = PeopleImageDownloader.new
   end
 
+  # NOTE excessive stubbing/mocking is a bit of a smell... counteract with integration
   it "parses out the image" do
+    blob  = Object.new
+    image = Object.new
+
+    mock(MechanizeProxy).new.mock! do |proxy|
+      proxy.cache_subdirectory = is_a(String)
+      proxy.get('/parlInfo/download/handbook/allmps/0J4/upload_ref_binary/0j4.jpg').mock!.body {blob}
+    end
+    mock(Magick::Image).from_blob( blob ).mock![0].returns(image)
+
+    @downloader = PeopleImageDownloader.new
+
     @downloader.extract_image(@snippet).should_not be_nil
   end
 end
