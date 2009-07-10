294184f34768e9f045b58c47eb430a83dc24c502
lachie <lachiec@gmail.com>
2009-07-10 15:11:02 +1000

    removed some cruft

--- lib/person/image_couch_loader.rb
+++ lib/person/image_couch_loader.rb
@@ -18,30 +18,17 @@ class Person
 		end
 
 		def add_attachment(person_doc,name,image)
-      #if person_doc['_attachments'][name]
-
       if @ok
         person_doc = person_doc.update('_rev' => @ok['rev'])
       end
 
-      puts 
-      
-      puts @db.send(:uri_for_attachment, person_doc, name)
-
-			#person_doc.send method, :name => name, :file => File.new(image.path)
       @ok = @db.put_attachment(person_doc, name, image.to_blob, :content_type => 'image/jpeg')
-
-      #as = person_doc['_attachments'] ||= {}
-      #a  = as[name] ||= {}
-      #a['content_type'] = 'image/jpeg'
-      #a['data']         = Base64.encode64(image.to_blob)
 		end
 
 		def output_image(person,image)
 			puts "fetching for #{person.couch_id}"
 
 			person_doc = @db.get(person.couch_id)
-			#person_doc.extend CouchRest::Mixins::ExtendedAttachments
 
       @ok = nil
 
@@ -55,17 +42,6 @@ class Person
 
 			image.resize("%dx%d" % [@@SMALL_THUMBNAIL_WIDTH    , @@SMALL_THUMBNAIL_HEIGHT]    )
 			add_attachment(person_doc, 'image-small', image)
-
-			#a = person_doc["_attachments"]
-			#a.keys.each do |k|
-			#	a[k].delete('content-type')
-			#	a[k]['content_type'] = mime_type
-			#end
-
-			#pp a
-
-			# @db.save_doc(person_doc)
-      
 
 		rescue RestClient::RequestFailed
 			puts "failed to put attachment"
