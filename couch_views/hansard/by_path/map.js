9f5cf5897f262e7cd96261ae1b55dc276b07453f
lachie <lachiec@gmail.com>
2009-07-12 23:22:19 +1000

    further refined view hansard/by_path

--- couch_views/hansard/by_path/map.js
+++ couch_views/hansard/by_path/map.js
@@ -1,7 +1,11 @@
 function(doc) {
   if(doc['hansard-tree']) {
     if(doc['path'])
-      emit(doc['path'],doc['title'])
+
+      if(doc['type'] == 'speech')
+        emit(doc['path'],'Speech by '+doc['speaker'])
+      else
+        emit(doc['path'],(doc['title'] || ''))
 
     else
       emit([],null)
